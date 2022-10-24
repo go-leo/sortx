a6dc21a5b992e0b8d2fcb111e9321b658c7d6870
soyacen <soyacen@outlook.com>
2022-10-24 10:07:13 +0800

    / add code

--- sort.go
+++ sort.go
@@ -1,24 +1,33 @@
 package sortx
 
 import (
+	"sort"
+
 	"golang.org/x/exp/constraints"
-	"golang.org/x/exp/slices"
 )
 
 // Asc sorts a slice of any ordered type in ascending order.
 func Asc[E constraints.Ordered](x []E) {
-	slices.Sort(x)
+	sort.Slice(x, func(i, j int) bool {
+		return x[i] < x[j]
+	})
 }
 
 // Desc sorts a slice of any ordered type in descending order.
 func Desc[E constraints.Ordered](x []E) {
-	slices.Sort(x)
-	Reverse(x, 0, len(x))
+	sort.Slice(x, func(i, j int) bool {
+		return x[i] > x[j]
+	})
 }
 
 // IsAsc reports whether x is sorted in ascending order.
 func IsAsc[E constraints.Ordered](x []E) bool {
-	return slices.IsSorted(x)
+	for i := len(x) - 1; i > 0; i-- {
+		if x[i] < x[i-1] {
+			return false
+		}
+	}
+	return true
 }
 
 // IsDesc reports whether x is sorted in descending order.
